bdc96335187ae99c6ba20369010d37c46b9a15bd
Tamim-Amin <tamimamin13579@gmail.com>
2026-02-05 11:33:54 +0600

    first commit

---
+++ Sphere_Volume_Calculator.py
@@ -0,0 +1,5 @@
+# Write a Python program to get the volume of a sphere with radius six.
+pi=3.1416
+radius=input("Enter the radius of the sphere: ")
+volume=(4/3)*pi*float(radius)**3
+print("The volume of the sphere with radius", radius, "is:", volume)
